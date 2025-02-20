00a7003904158293d177243ea2c28bf168fdf6ab
Daniel Opara <58013370+bimmui@users.noreply.github.com>
2025-02-19 21:30:00 -0500

    updated dynamical model of weather balloon

--- PID-playground/acs_sim.py
+++ PID-playground/acs_sim.py
@@ -1,27 +1,115 @@
 from pid import PIDController
 from ambiance import Atmosphere
 import pandas as pd
+import numpy as np
+
 
 # constants
 ACS_ACTIVATE_ALTITUDE = 30000  # meters
 ZERO_VERTICAL_VELOCITY = 0
-BALLOON_MASS = 5
-EARTH_GRAV_ACCEL = 9.81
+g = 9.81 # m/s^2
+c = 0 # this needs to be changed but idk what its supposed to be
+Psl0 = 101325 # pascals
+Psl2 = 2481 # pascals
+a_valve = 0.5 # meters, filler so change this 
+latex_elastic_stress = 0.1 # pascals, filler so change this 
+rho_helium = 7 # kg / meters^3, filler so change this 
+
+
+class Balloon:
+    """
+    Initializes the Balloon class with initial state parameters and
+    contains the dynamic state variables updated through the simulation.
+
+    :param init_alt: Initial altitude in meters
+    :param init_velo: Initial velocity in meters per sec
+    :param init_r: Initial radius of the balloon in meters
+    :param init_r: Mass of the balloon payload
+    """
+    def __init__(self, init_alt, init_velo, init_r, final_r, m):
+        self.curr_velo = init_velo
+        self.init_r = init_r
+        self.final_r = final_r
+        self.mass = m
+        self.curr_pressure = 0
+        self.curr_volume = 0
+        self.curr_radius = 0
+        self.curr_density = 0
+        self.valve_volumetric_flow_rate = 0
+        self.cross_section_area = 0 
+
+
+    
+    def update(self, altitude, delta_t):
+        # TODO: update the curr_velocity of the balloon as well, do it in here or in another function
+        atmosphere = Atmosphere(altitude)
+
+        # a little bit standalone compared to the other calcs that are dependent on each other
+        self.cross_section_area = self.calc_balloon_cross_section_area(self.curr_radius)
+
+        self.curr_pressure = self.calc_balloon_pressure(atmosphere.pressure, self.curr_radius)
+        self.valve_volumetric_flow_rate = self.calc_balloon_dV(a_valve, atmosphere.pressure, rho_helium)
+        self.curr_volume = self.calc_balloon_volume(rho_helium, delta_t)
+        self.curr_density = self.calc_balloon_density(rho_helium, delta_t)
+        self.cross_section_area = self.calc_balloon_cross_section_area(self.curr_radius)
+
+        # updating balloon state values for next iteration
+        self.curr_radius = np.cbrt((3 * self.curr_volume) / (4 * np.pi))
+
+
+
+
+    
+    def calc_balloon_cross_section_area(self):
+        """
+        Calculate the cross-sectional area of the balloon.
+        NOTE: drag force requires cross sectional area of the balloon which changes with its radius r
+        """
+        return np.pi * self.curr_radius**2
+
+    def calc_balloon_pressure(self, pressure_atmo):
+        """Calculate the balloon's internal pressure"""
+        return pressure_atmo + ((2 * latex_elastic_stress) / self.curr_radius)
+
+    def calc_balloon_dV(self, A_valve, pressure_atmo, rho_helium):
+        """Calculate the differential volume change through the valve"""
+        return A_valve * np.sqrt(2 * ((self.curr_pressure - pressure_atmo) / rho_helium))
+
+    def calc_balloon_volume(self, pressure_atmo, delta_t):
+        """Calculate the balloon's volume"""
+        # change that c constant idk what it is
+        return ((4/3) * np.pi) * (c + ((self.final_r - self.init_r) / (Psl0 - Psl2)) * pressure_atmo) - (self.valve_volumetric_flow_rate * delta_t)
+
+    def calc_balloon_density(self, rho_helium, delta_t):
+        """Calculate the balloon's density"""
+        return (((4/3) * np.pi * self.init_r**3 * rho_helium) - (self.curr_pressure * self.valve_volumetric_flow_rate * delta_t)) / self.curr_volume
+
 
 # state variables
 position_pid_active = False
 vert_velocity_pid_active = False
 descending = False
 
-# dynamical model state variables
-curr_alt = None
-curr_vert_velo = None
-balloon_radius = None
+
+def force_drag(C_d, rho_atmo, balloon_obj):
+    """Calculate the drag force on the balloon"""
+    # Assuming multiplication between A_d and ((-1 * v) * abs(v))
+    return ((C_d * rho_atmo * balloon_obj.cross_section_area) * ((-1 * balloon_obj.curr_velo) * abs(balloon_obj.curr_velo))) / 2
+
+def force_lift(rho_atmo, balloon_obj):
+    """Calculate the lift force on the balloon"""
+    return (g * balloon_obj.curr_volume) * (rho_atmo - balloon_obj.curr_density)
+
+def force_gravity(balloon_obj):
+    """Calculate the force of gravity on the balloon"""
+    return balloon_obj.mass * g
+
+def dvdt(balloon, altitude, delta_t):
+    atmosphere = Atmosphere(altitude)
+    balloon.update(altitude, delta_t)
+    return (force_lift(atmosphere.density, balloon) - force_gravity(balloon) + force_drag(0.5, atmosphere.density, balloon)) / balloon.mass
 
 
-
-balloon_volume = lambda r_0, rho_atmo, T, n: r_0 * (1/rho_atmo)**(1/3)
-dvdt = lambda V_b, rho_atmo, rho_b, g, m, C_D, S, v: ((V_b * (rho_atmo - rho_b) * g) - (mg) + (0.5 * C_D * rho_atmo * S * v**2) / m)
 
 
 hab_data = pd.read_csv("data/PID-playground/data/24057799_alt-ascent-descent.csv")
